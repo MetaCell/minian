5a640e7c6ac95c2530da5c58643d3bda9a093b97
Zoran Sinnema <zsinnema@gmail.com>
2020-09-10 08:33:33 +0200

    Bump version to v0.2.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def _get_requirements(requirement_file):
 
 setuptools.setup(
     name=NAME,
-    version=VERSION,
+    version='0.2.3',
     author="Denise Cai",
     author_email="denisecai@gmail.com",
     description="MiniAn is an analysis pipeline and visualization tool inspired by both CaImAn and MIN1PIPE package specifically for Miniscope data.",
